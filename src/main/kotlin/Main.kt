3c6c2b7c46cf2a174bdfffc1e7d88f72b852fc9d
nodari-dev <nodari.pylypyshak@gmail.com>
2023-08-27 01:22:17 +0200

    regex + more logic for data analyzer

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,7 +3,7 @@ import fetcher.Fetcher
 
 fun main() {
 //    CommunicationManager.startCrawling(listOf("https://ecospace.org.ua"))
-    val html = Fetcher().getPageHTML("https://ecospace.org.ua/sea-en/")
+    val html = Fetcher().getPageHTML("https://ecospace.org.ua/sea")
     if(html != null){
         DataAnalyzer().test(html)
     }
