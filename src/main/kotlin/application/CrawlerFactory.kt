ec3121a1edd7cb5b2e19cedcb9b387ca74744b64
nodari-dev <nodari.pylypyshak@gmail.com>
2023-11-03 16:33:05 +0100

    dip

--- src/main/kotlin/application/CrawlerFactory.kt
+++ src/main/kotlin/application/CrawlerFactory.kt
@@ -4,9 +4,9 @@ import application.crawler.CrawlerV2
 import application.crawler.entities.CrawlerConfig
 import application.interfaces.ICrawlerFactory
 import application.interfaces.ICrawlerV2
-import storage.frontier.FrontierV2
+import storage.interfaces.IFrontierV2
 
-class CrawlerFactory(private val frontier: FrontierV2): ICrawlerFactory {
+class CrawlerFactory(private val frontier: IFrontierV2): ICrawlerFactory {
     override fun generateCrawler(config: CrawlerConfig): ICrawlerV2 {
         return CrawlerV2(config, frontier)
     }
