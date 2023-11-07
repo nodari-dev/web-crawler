8151bf4ee70f7f1fa303935433e5c7d85c097aaa
nodari-dev <nodari.pylypyshak@gmail.com>
2023-11-07 16:22:19 +0100

    removed commits

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,6 @@
 import application.parser.urlparser.URLParser
 import infrastructure.repository.FrontierRepository
+import mu.KotlinLogging
 import redis.clients.jedis.JedisPool
 import storage.frontier.FrontierV2
 import java.util.concurrent.locks.ReentrantLock
@@ -11,36 +12,11 @@ fun main() {
     val reentrantLock = ReentrantLock()
 
     val jedis = JedisPool("localhost", 6379).resource
-//    jedis.rpush(host, url)
-//    jedis.hset("frontier", "queues", host)
-//    val lists = jedis.hget("frontier", "queues")
-//    println(lists.contains("not_exist"))
-//    println(lists.contains(url))
-//
-//    val list = jedis.hget("frontier", "queues")
-//    println(jedis.lrange(list, 0, -1))
-//    val result = jedis.lpop(list)
-//    println(jedis.lrange(list, 0, -1))
-//    println("result $result")
-
-
-
 
     val frontierRepository = FrontierRepository(reentrantLock, jedis)
-//
-//    val frontier = FrontierV2(frontierRepository)
-//    frontier.updateOrCreateQueue(host, listOf(url))
-//    println(frontier.pullWebLink(host))
-//    frontier.deleteQueue(host)
+    val kotlinLogger = KotlinLogging.logger("Frontier")
+    val frontier = FrontierV2(frontierRepository, kotlinLogger)
 
-    val expectedResult = mutableMapOf(
-        "host-urls" to "host",
-        "host-crawlers" to "host-crawlers"
-    )
-
-    println(
-        expectedResult.filter { item -> item.key.contains("crawlers") }
-    )
 //    val fetcher = Fetcher(KotlinLogging.logger("fetcher"))
 //    val urlParser = URLParser()
 //
